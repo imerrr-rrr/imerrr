5f56f0caed3004885fc016c072c1d8da8fcaed48
imerrr-rrr <imerguevara98@gmail.com>
2026-05-18 15:24:03 -0600

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,10 @@
+while True:
+    print ("l-argentina")
+    print ("2-francia")
+    print ("3-españa")
+    numero = int(input("cual es el mejor equipo?"))
+    if numero == 2:
+        print ("campeon")
+    else:
+        print("subcampeon")
+    break
